8b01e08476b3ba5b1dd31908be6f8a516e0968f1
Tyanne Jensen <vibewithme1123@Tyannes-Laptop.local>
2024-11-19 22:33:15 -0700

    commented out code so that we could get the app running, added a log in compenent and route.

---
+++ server/dist/seeds/tip-seeds.js
@@ -0,0 +1,15 @@
+import { Tip } from '../models/tips.js';
+export const seedTips = async () => {
+    await Tip.bulkCreate([
+        {
+            username: 'JJ',
+            topic: 'UX',
+            tip: "Don't use Papyrus"
+        },
+        {
+            username: 'Sarah',
+            topic: 'UX',
+            tip: "Use alt tags to improve accessibility"
+        },
+    ]);
+};
