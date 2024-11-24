22e0f7bc577efb3005ddf2165ff0fbc84be5b3f3
Tyanne Jensen <vibewithme1123@Tyannes-Laptop.local>
2024-11-23 20:25:46 -0700

    added login and register page, added auth gaurd, set up local storage.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,46 +1,34 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-import App from './App.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import Home from './pages/Home.tsx';
-import Feedback from './pages/Feedback.tsx';
-import SingleTip from './pages/SingleTip.tsx';
-import SingleFeedback from './pages/SingleFeedback.tsx';
-import Login from './components/login.tsx';
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//       },
+//       {
+//         path: "/Register",
+//         element: <Register />,
+//       },
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//     ],
+//   },
+// ]);
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      }, 
-      {
-        path: '/log-in',
-        element: <Login/>
-      },
-      {
-        path: '/feedback',
-        element: <Feedback />,
-      },
-      {
-        path: '/tip/:id',
-        element: <SingleTip />,
-      },
-      {
-        path: '/feedback/:id',
-        element: <SingleFeedback />,
-      },
-    ],
-  },
-]);
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+  ReactDOM.createRoot(rootElement).render(<App />);
 }
