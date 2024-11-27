88d6176dcb1f852fc69bd786e46f6bdba4a7ee61
Skylar Kline <skylarkline16@gmail.com>
2024-11-26 17:48:24 -0800

    Added starter catcha API files, and created FeedbackForm (edit to app.tsx and home.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import Register from "./components/Register";
 import Home from "./components/Home";
 import { getActiveUser } from "./LocalStorage";
 import Navbar from "./components/Navbar/Navbar";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   return (
@@ -36,3 +37,50 @@ const PrivateRoute = () => {
 };
 
 export default App;
+
+
+// Skylark Changes for FeedbackForm
+// import {
+//   BrowserRouter,
+//   Navigate,
+//   Outlet,
+//   Route,
+//   Routes,
+// } from "react-router-dom";
+// import "./App.css";
+// import Login from "./components/login";
+// import Register from "./components/Register";
+// import Home from "./components/Home";
+// import ContactForm from "./components/ContactForm"; // Import the ContactForm component
+// import { getActiveUser } from "./LocalStorage";
+// import Navbar from "./components/Navbar/Navbar";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar /> {/* Moved inside BrowserRouter for better routing behavior */}
+//       <Routes>
+//         {/* Public routes */}
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+
+//         {/* Private route */}
+//         <Route element={<PrivateRoute />}>
+//           <Route path="/" element={<Home />} />
+//         </Route>
+
+//         {/* New Contact route (public) */}
+//         <Route path="/contact" element={<ContactForm />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// const PrivateRoute = () => {
+//   const activeUser = getActiveUser();
+//   if (activeUser === null) return <Navigate to="/login" />;
+
+//   return <Outlet />;
+// };
+
+// export default App;
