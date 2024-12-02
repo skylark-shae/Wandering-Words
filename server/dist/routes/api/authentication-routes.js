2a4e73f25adb5e45c051d0c83cbe200ce447c216
Tyanne Jensen <vibewithme1123@Tyannes-Laptop.local>
2024-12-01 21:26:19 -0700

    removed dists

--- server/dist/routes/api/authentication-routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { register, login, logout, getNewAccessToken } from '../../controllers/authentication-controller.js';
-const router = express.Router();
-router.post('/login', login);
-router.post('/register', register);
-router.post('/refreshToken', getNewAccessToken);
-router.delete('/refreshToken', logout);
-export { router as authenticationRouter };
--- server/dist/routes/api/chat-gpt-routes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { chat } from '../../controllers/open-ai-chat-controller.js';
-import auth from '../../middleware/auth.js';
-const router = express.Router();
-router.post('/', auth, chat);
-export { router as openAiRouter };
