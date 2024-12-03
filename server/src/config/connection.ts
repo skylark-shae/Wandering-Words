dd693e95651cf294c81099114a4bb935b0359b93
Tyanne Jensen <vibewithme1123@Tyannes-Laptop.local>
2024-12-02 21:35:24 -0700

    Updated all the things

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,14 +1,13 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-
-import pg from 'pg';
+import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  host: 'localhost',
+  host: "localhost",
   database: process.env.DB_NAME,
   port: 5432,
 });
@@ -16,24 +15,18 @@ const pool = new Pool({
 const connectToDb = async () => {
   try {
     await pool.connect();
-    console.log('Connected to the database.');
+    console.log("Connected to the database.");
   } catch (err) {
-    console.error('Error connecting to database:', err);
+    console.error("Error connecting to database:", err);
     process.exit(1);
   }
 };
 
 export { pool, connectToDb };
 
-import { Sequelize } from 'sequelize';
-
-export default new Sequelize(
-    process.env.DB_NAME as string, 
-    process.env.DB_USER as string, 
-    process.env.DB_PASSWORD as string, 
-    {
-      host: process.env.DB_HOST as string,
-      dialect: 'postgres',
-      logging: false, // Turn off SQL logging
-    }
-  );
+import { Sequelize } from "sequelize";
+let dbConnectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+if (process.env.DB_HOST !== "localhost") {
+  dbConnectionString += "?ssl=true";
+}
+export default new Sequelize(dbConnectionString);
